30fa2043c995ee74daf7c572896d1d2061777d23
Joel Arias <joelariasparra3@gmail.com>
2024-11-10 13:45:02 -0300

    (Update) Migracion de paginas v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,14 +5,11 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "../shared/navbar/navbar.component";
 import { FooterComponent } from '../shared/footer/footer.component';
 
-// Pages
-import { HomeComponent } from '../pages/home/home.component';
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, HomeComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
